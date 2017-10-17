951e94283930bd668ee376a6e9d774eaaad44188
Jorje196 <geo.kir.61@gmail.com>
2017-10-17 23:22:02 +0300

    Создана стартовая версия списка Ignore

---
+++ app/src/main/java/jorje196/com/github/brewmaster/MainBeerActivity.java
@@ -0,0 +1,13 @@
+package jorje196.com.github.brewmaster;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainBeerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_beer);
+    }
+}
