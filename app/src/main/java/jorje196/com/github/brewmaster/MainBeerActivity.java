6919c6fd8d5afa63a69e5a926fa78e41b2457667
Jorje196 <geo.kir.61@gmail.com>
2017-10-25 00:00:17 +0300

    На Top добавлены присказки

--- app/src/main/java/jorje196/com/github/brewmaster/MainBeerActivity.java
+++ app/src/main/java/jorje196/com/github/brewmaster/MainBeerActivity.java
@@ -2,6 +2,7 @@ package jorje196.com.github.brewmaster;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainBeerActivity extends Activity {
 
@@ -9,5 +10,13 @@ public class MainBeerActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_beer);
+        // получаем ресурс
+        String[] top_aphorisms = getResources().getStringArray(R.array.aphorisms);
+        // выбираем присказку
+        int num_saying;
+        num_saying = (int)(Math.random()*top_aphorisms.length);
+        TextView aphorismView = (TextView) findViewById(R.id.top_aphorism);
+        aphorismView.setText(top_aphorisms[num_saying]);
+
     }
 }
