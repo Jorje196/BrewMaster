6851907d795214c80560dd5a0ba9691322e01043
Jorje196 <geo.kir.61@gmail.com>
2017-10-27 13:48:46 +0300

    Определены пункты меню действий, поставлены заглушки true на обработку

--- app/src/main/java/jorje196/com/github/brewmaster/MainBeerActivity.java
+++ app/src/main/java/jorje196/com/github/brewmaster/MainBeerActivity.java
@@ -2,6 +2,8 @@ package jorje196.com.github.brewmaster;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainBeerActivity extends Activity {
@@ -12,11 +14,39 @@ public class MainBeerActivity extends Activity {
         setContentView(R.layout.activity_main_beer);
         // получаем ресурс
         String[] top_aphorisms = getResources().getStringArray(R.array.aphorisms);
-        // выбираем присказку
+        // выбираем присказку , картинка пока безальтернативна
         int num_saying;
-        num_saying = (int)(Math.random()*top_aphorisms.length);
+        num_saying = (int) (Math.random() * top_aphorisms.length);
         TextView aphorismView = (TextView) findViewById(R.id.top_aphorism);
         aphorismView.setText(top_aphorisms[num_saying]);
+    }
 
+    //для отображения меню на панели действий реализуем метод onCreateOptionsMenu()
+    // элементы действий берем из файла ресурсов xml
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_of_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // выполняется когда выбирается элемент на панели действий, получает объект MenuItem
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                return true;
+            case R.id.action_brew_list:
+                return true;
+            case R.id.action_edit_brew:
+                return true;
+            case R.id.action_create_brew:
+                return true;
+            case R.id.action_settings:
+                return true;
+            case R.id.action_saving:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
