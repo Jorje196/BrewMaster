5abb75076429014af506358a51bb3613c58b6ec8
Jorje196 <geo.kir.61@gmail.com>
2017-11-22 10:41:52 +0300

    Фрагменты для активности верхнего уровня добавлены, пока много заглушек

---
+++ app/src/main/java/jorje196/com/github/brewmaster/DescriptionsListFragment.java
@@ -0,0 +1,29 @@
+package jorje196.com.github.brewmaster;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DescriptionsFragment extends Fragment {
+
+
+    public DescriptionsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_descriptions, container, false);
+    }
+
+}
