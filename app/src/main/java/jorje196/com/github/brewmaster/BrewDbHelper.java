437297faa8c2c231aa36bee1dbb9d7154caf3ec4
Jorje196 <geo.kir.61@gmail.com>
2018-01-03 20:02:15 +0300

    Добавлен фрагмент для работы со списком варок , обе ориентации .

--- app/src/main/java/jorje196/com/github/brewmaster/BrewDbHelper.java
+++ app/src/main/java/jorje196/com/github/brewmaster/BrewDbHelper.java
@@ -5,6 +5,8 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import static android.app.PendingIntent.getActivity;
+
 /**
  * Created by User on 01.11.2017.
  * Helper для создания и управления базой данных
@@ -13,9 +15,11 @@ import android.database.sqlite.SQLiteOpenHelper;
 class BrewDbHelper extends SQLiteOpenHelper {
     private static final String DB_NAME = "BrewDb.db";
     private static final int DB_VERSION = 1;
-
+    private Context ctx ;
     BrewDbHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
+        // Для обеспечени возможности обращаться к ресурсам в onCreate & Co
+        ctx = context;  // String exp = ctx.getResources().getString(R.string.bitter);
     }
 
     @Override
@@ -35,6 +39,8 @@ class BrewDbHelper extends SQLiteOpenHelper {
             db.execSQL(DbContract.DbVerieties.CREATE_TABLE_VERIETIES);
 
             db.execSQL(DbContract.DbBrews.CREATE_TABLE_BREWS);
+
+            db.execSQL(DbContract.DbStates.CREATE_TABLE_STATES);
 
 
         // первичное заполнение таблиц
@@ -63,9 +69,11 @@ class BrewDbHelper extends SQLiteOpenHelper {
             DbContract.DbBrands.insertBrand(db, "Inpinto");
 
                 // загустители
+            DbContract.DbThickeners.insertThickener(db, "Without thickener");
             DbContract.DbThickeners.insertThickener(db, "maltodextrin");
 
                 // усилители
+            DbContract.DbEnhancers.insertEnhancer(db, "Without enhancer");
             DbContract.DbEnhancers.insertEnhancer(db, "Coopers Light Dry Malt");
             DbContract.DbEnhancers.insertEnhancer(db, "Muntons Light Malt Extract");
             DbContract.DbEnhancers.insertEnhancer(db, "Muntons Dark Malt Extract");
@@ -75,86 +83,149 @@ class BrewDbHelper extends SQLiteOpenHelper {
             DbContract.DbCans.insertCans(db, 1.8, 23.);
             DbContract.DbCans.insertCans(db, 1.5, 18.);
 
+                // состояния процесса
+
+            DbContract.DbStates.insertState(db, ctx.getResources().getString(R.string.in_progress));
+            DbContract.DbStates.insertState(db, ctx.getResources().getString(R.string.complited));
+
 
                 // итоговая (brand+name) таблица характеристик
-            DbContract.DbVerieties.insertVerieties(db, "Draught", "Coopers",
+            DbContract.DbVerieties.insertVerieties(db, "Draught", "Coopers",    //1
                     420, 130, 1, "draught_small",
                     "http://store.coopers.com.au/draught-1-7kg.html",
                     "Светлое слабо горькое , скорее летнее, рыба, белая птица");
-            DbContract.DbVerieties.insertVerieties(db, "Draught", "Muntons",
+            DbContract.DbVerieties.insertVerieties(db, "Draught", "Muntons",    //2
                     420, 130, 2, "draught_small",
                     "http://store.coopers.com.au/draught-1-7kg.html",
                     "Светлое слабо горькое , скорее летнее, рыба, белая птица");
-            DbContract.DbVerieties.insertVerieties(db, "Draught", "Finlandia",
+            DbContract.DbVerieties.insertVerieties(db, "Draught", "Finlandia",  //3
                     420, 130, 3, "draught_small",
                     "http://store.coopers.com.au/draught-1-7kg.html",
                     "Светлое слабо горькое , скорее летнее, рыба, белая птица");
-            DbContract.DbVerieties.insertVerieties(db, "Lager", "Coopers",
+            DbContract.DbVerieties.insertVerieties(db, "Lager", "Coopers",      //4
                     390, 75, 1, "lager_small",
                     "http://store.coopers.com.au/lager-1-7kg.html",
                     "Очень светлое слабо горькое , летнее, рыба, белая птица");
-            DbContract.DbVerieties.insertVerieties(db, "English Bitter", "Coopers",
+            DbContract.DbVerieties.insertVerieties(db, "English Bitter", "Coopers", //5
                     620, 420, 1, "english_bitter_small",
                     "http://store.coopers.com.au/english-bitter-1-7kg.html",
                     "Горькое, универсальное по сезону, ближе к мясу с соответствующими соусами и гарниром");
-            DbContract.DbVerieties.insertVerieties(db, "Real Ale", "Coopers",
+            DbContract.DbVerieties.insertVerieties(db, "Real Ale", "Coopers",       //6
                     560, 230, 1, "real_ale_small",
                     "http://store.coopers.com.au/real-ale-1-7kg.html",
                     "Среднее горьковатое , универсальное по сезону, мясо, птица");
-            DbContract.DbVerieties.insertVerieties(db, "Dark Ale", "Coopers",
+            DbContract.DbVerieties.insertVerieties(db, "Dark Ale", "Coopers",       //7
                     590, 650, 1, "dark_ale_small",
                     "http://store.coopers.com.au/dark-ale.html",
                     "Горькое, темное, универсальное по сезону, но приятнее зимой и к мясу ");
-            DbContract.DbVerieties.insertVerieties(db, "Stout", "Coopers",
+            DbContract.DbVerieties.insertVerieties(db, "Stout", "Coopers",          //8
                     710, 1800, 1, "stout_small",
                     "http://store.coopers.com.au/stout-1-7kg.html",
                     "Стаут, универсальное по сезону, но особенно хорош зимним вечером ");
-            DbContract.DbVerieties.insertVerieties(db, "Indian Pale Ale (IPA)", "Coopers",
+            DbContract.DbVerieties.insertVerieties(db, "Indian Pale Ale (IPA)", "Coopers",  //9
                     830, 230, 1, "brew_a_ipa_small",
                     "http://store.coopers.com.au/thomas-coopers-brew-a-ipa-1-7kg.html",
                     "Горькое, самодостаточное в любое время года, если к мясу, то на гриле");
-            DbContract.DbVerieties.insertVerieties(db, "Wheat Beer", "Muntons",
+            DbContract.DbVerieties.insertVerieties(db, "Wheat Beer", "Muntons",             //10
                     30, 8, 1, "mexican_cerveza_small",
                     "http://www.muntonshomebrew.com/wp-content/uploads/2012/09/connoisseur_wheat_beer.jpg",
                     "Пшеничное, скорее летнее, самодостаточно, на любителя белая птица");
-            DbContract.DbVerieties.insertVerieties(db, "Preacher's Hefe Wheat", "Coopers",
+            DbContract.DbVerieties.insertVerieties(db, "Preacher's Hefe Wheat", "Coopers",      //11
                     340, 65, 1, "preacher_s_hefe_wheat_small",
                     "http://store.coopers.com.au/thomas-coopers-preacher-s-hefe-wheat-1-7kg.html",
                     "Пшеничное, скорее летнее, самодостаточно, на любителя белая птица и пряности");
-            DbContract.DbVerieties.insertVerieties(db, "Australian Pale Ale", "Coopers",
+            DbContract.DbVerieties.insertVerieties(db, "Australian Pale Ale", "Coopers",        //12
                     340, 90, 1, "australian_pale_ale_small",
                     "http://store.coopers.com.au/australian-pale-ale-1-7kg.html",
                     "Светлое, скорее летнее, средней горечи, хорошо уживается с любым темным мясом");
-            DbContract.DbVerieties.insertVerieties(db, "Canadian Blonde", "Coopers",
+            DbContract.DbVerieties.insertVerieties(db, "Canadian Blonde", "Coopers",                //13
                     420, 55, 1, "canadian_blonde_small",
                     "http://store.coopers.com.au/canadian-blonde-1-7kg.html",
-                    "светлое, скорее летнее, средней горечи, хорошо уживается с любым темным мясом");
-            DbContract.DbVerieties.insertVerieties(db, "European Lager", "Coopers",
+                    "Светлое, скорее летнее, средней горечи, хорошо уживается с любым темным мясом");
+            DbContract.DbVerieties.insertVerieties(db, "European Lager", "Coopers",                 //14
                     340, 90, 1, "european_lager_small",
                     "http://store.coopers.com.au/european-lager-1-7kg.html",
-                    "светлое, скорее летнее, средней горечи, предпочтительно к рыбе или птице");
-            DbContract.DbVerieties.insertVerieties(db, "Mexican Cerveza", "Coopers",
+                    "Светлое, скорее летнее, средней горечи, предпочтительно к рыбе или птице");
+            DbContract.DbVerieties.insertVerieties(db, "Mexican Cerveza", "Coopers",                //15
                     420, 53, 1, "mexican_cerveza_small",
                     "http://store.coopers.com.au/mexican-cerveza-1-7kg.html",
-                    "светлое, летнее, средней горечи со сладковатым привкусом, хорошо сочетается с " +
+                    "Светлое, летнее, средней горечи со сладковатым привкусом, хорошо сочетается с " +
                             "'острыми' блюдами");
-            DbContract.DbVerieties.insertVerieties(db, "Irish Stout", "Coopers",
+            DbContract.DbVerieties.insertVerieties(db, "Irish Stout", "Coopers",                       //16
                     560, 1650, 1, "irish_stout_small",
                     "http://store.coopers.com.au/irish-stout-1-7kg.html",
-                    "очень темое, абсолютно самодостаточно, хорошо сочетается с " +
+                    "Очень темое, абсолютно самодостаточно, хорошо сочетается с " +
                             "холодными зимними вечерами, промозглая погода тоже подходит. " +
                             "но употреблять строго во второй половине дня !");
 
             // TODO может длинные вынести в string-ресурс ?
 
                 // варки
-            DbContract.DbBrews.insertBrews(db, 1, 23.0, "2016-09-23 17ч", 9.0, 30, 0.5);
-            DbContract.DbBrews.insertBrews(db, 2, 23.0, "2016-10-12 12ч", 9.5, 28, 1.0);
-            DbContract.DbBrews.insertBrews(db, 3, 23.0, "2016-10-21:11ч", 9.5, 27, 1.0);
-            DbContract.DbBrews.insertBrews(db, 4, 23.0, "2016-11-03:19ч", 9.0, 26, 1.5);
-            DbContract.DbBrews.insertBrews(db, 5, 23.0, "2016-11-20:13ч", 10.5, 28, 1.0);
-            DbContract.DbBrews.insertBrews(db, 6, 23.0, "2016-12-01:23ч", 10.0, 29, 1.5);
+            DbContract.DbBrews.insertBrews(db, 6, 25.5, "2015-09-18", 8.0, 26, 1.25,
+                    23, "2015-09-25", 3.5, "2015-10-11", 2, 0,
+                    1.0, 1, 0, 1, 0, "Чтой-то перелив вышел", 0);
 
+            DbContract.DbBrews.insertBrews(db, 7, 23.5, "2015-09-27", 8.0, 29, 1.0,
+                    23, "2015-10-04", 3.4, "2015-10-20", 2, 0,
+                    1.0, 1, 0.0, 1, 0.0, "", 0);
+
+            DbContract.DbBrews.insertBrews(db, 8, 22.0, "2015-11-07", 10.0, 25, 1.5,
+                    22, "2015-11-13", 4.5, "2015-11-27", 2, 0,
+                    1.0, 1, 0, 1, 0, " Закопать до декабря 2016 !", 0);
+
+            DbContract.DbBrews.insertBrews(db, 14, 24.0, "2015-11-13", 8.5, 26, 2.0,
+                    22, "2015-11-24", 3.25, "2015-12-11", 2, 0.0,
+                    1.0,1, 0, 1, 0, "Best after : февраль 2016", 0);
+
+            DbContract.DbBrews.insertBrews(db, 11, 23.0, "2015-12-16", 10.5, 28, 1.0,
+                    23, "2015-12-25", 3.75, "2016-01-14", 2, 0.0,
+                    1.0, 1, 0, 1, 0, "!? 5 литров ушло в пропасть.", 0);
+
+            DbContract.DbBrews.insertBrews(db, 7, 24.0, "2015-12-26", 8.0, 27, 1.0,
+                    20, "2016-01-05", 4, "2016-01-20", 2, 0,
+                    1.0, 1, 0.0, 1, 0.0, "Норм. Поток.", 0);
+
+            DbContract.DbBrews.insertBrews(db, 9, 24.0, "2016-01-05", 9.0, 26, 1.5,
+                     20,"2016-01-13", 4.25, "2016-01-21", 2, 0,
+                     1.0, 1, 0, 1, 0, "Последняя варка в серии", 0);
+
+            DbContract.DbBrews.insertBrews(db, 6, 24, "2016-05-13", 9.0, 28, 1.0,
+                    23, "2016-05-19", 4.0, "2016-06-01", 1, 0,
+                    1.0, 1, 0, 1, 0, "Летний почин", 0);
+
+            DbContract.DbBrews.insertBrews(db, 1, 23.0, "2016-05-20", 9.0, 26, 1.5,
+                    23, "2016-05-26", 4.25, "2016-06-10", 2, 0,
+                    1.0, 1,0, 1, 0, "Задел на август", 0);
+
+            DbContract.DbBrews.insertBrews(db, 14, 23.5, "2016-05-27", 9.0, 28, 1.0,
+                    25, "2016-06-03", 4.5, "2016-06-18", 2, 0.0,
+                    1.0,1, 0, 1, 0, "Выпить летом", 0);
+
+            DbContract.DbBrews.insertBrews(db, 12, 24.0, "2016-07-20", 8.5, 28, 1.0,
+                    27, "2016-07-24", 4.25, "2016-07-10", 2, 0.0,
+                    1.0,1, 0, 1, 0, "", 0);
+
+            DbContract.DbBrews.insertBrews(db, 6, 24.5, "2016-08-24", 8.5, 28, 1.0,
+                    26, "2016-08-29", 4.25, "2016-09-11", 2, 0,
+                    1.0, 1, 0, 1, 0, "По накатанной", 37);
+
+            DbContract.DbBrews.insertBrews(db, 7, 24.0, "2016-09-17", 8.5, 28, 1.0,
+                    22, "2016-09-26", 4.25, "2016-10-10", 2, 0,
+                    1.0, 1, 0.0, 1, 0.0, "Ждало 4 дня , был занят", 21);
+
+            DbContract.DbBrews.insertBrews(db, 9, 24.0, "2016-09-26", 9.0, 28, 1.0,
+                    20,"2016-01-13", 4.25, "2016-01-21", 1, 0,
+                    1.0, 1, 0, 1, 0, "Осенняя серия", 72);
+
+
+
+
+            /*
+            DbContract.DbBrews.insertBrews(db, 7, 23.0, "2016-09-23 17ч", 9.0, 30, 0.5);
+            DbContract.DbBrews.insertBrews(db, 8, 23.0, "2016-10-12 12ч", 9.5, 28, 1.0);
+            DbContract.DbBrews.insertBrews(db, 9, 23.0, "2016-10-21:11ч", 9.5, 27, 1.0);
+            DbContract.DbBrews.insertBrews(db, 14, 23.0, "2016-09-23 17ч", 9.0, 30, 0.5);
+            DbContract.DbBrews.insertBrews(db, 3, 23.0, "2016-10-21:11ч", 9.5, 28, 1.5); */
 
         } catch (SQLException e) {
             // exceptCount++;
--- app/src/main/java/jorje196/com/github/brewmaster/BrewListFragment.java
+++ app/src/main/java/jorje196/com/github/brewmaster/BrewListFragment.java
@@ -1,42 +1,161 @@
 package jorje196.com.github.brewmaster;
 
+import android.app.Activity;
 import android.app.ListFragment;
-import android.database.sqlite.SQLiteDatabase;
+import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+import static jorje196.com.github.brewmaster.DbContract.DbBrands.COLUMN_BRANDS_BRAND;
+import static jorje196.com.github.brewmaster.DbContract.DbBrands.COLUMN_BRANDS_ID;
+import static jorje196.com.github.brewmaster.DbContract.DbBrands.TABLE_BRANDS;
+import static jorje196.com.github.brewmaster.DbContract.DbBrews.COLUMN_BREWS_ALC_PERCENT;
+import static jorje196.com.github.brewmaster.DbContract.DbBrews.COLUMN_BREWS_BITTER;
+import static jorje196.com.github.brewmaster.DbContract.DbBrews.COLUMN_BREWS_ID;
+import static jorje196.com.github.brewmaster.DbContract.DbBrews.COLUMN_BREWS_PROCESS_STATE;
+import static jorje196.com.github.brewmaster.DbContract.DbBrews.COLUMN_BREWS_START_DATA;
+import static jorje196.com.github.brewmaster.DbContract.DbBrews.COLUMN_BREWS_VERIETY_ID;
+import static jorje196.com.github.brewmaster.DbContract.DbBrews.COLUMN_BREWS_VOLUME;
+import static jorje196.com.github.brewmaster.DbContract.DbBrews.TABLE_BREWS;
+import static jorje196.com.github.brewmaster.DbContract.DbCans.COLUMN_CANS_ID;
+import static jorje196.com.github.brewmaster.DbContract.DbCans.COLUMN_CANS_WEIGHT;
+import static jorje196.com.github.brewmaster.DbContract.DbCans.TABLE_CANS;
+import static jorje196.com.github.brewmaster.DbContract.DbNames.COLUMN_NAMES_ID;
+import static jorje196.com.github.brewmaster.DbContract.DbNames.COLUMN_NAMES_NAME;
+import static jorje196.com.github.brewmaster.DbContract.DbNames.TABLE_NAMES;
+import static jorje196.com.github.brewmaster.DbContract.DbStates.COLUMN_STATES_ID;
+import static jorje196.com.github.brewmaster.DbContract.DbStates.COLUMN_STATES_TEXT;
+import static jorje196.com.github.brewmaster.DbContract.DbStates.TABLE_STATES;
+import static jorje196.com.github.brewmaster.DbContract.DbVerieties.COLUMN_VERIETIES_BITTER;
+import static jorje196.com.github.brewmaster.DbContract.DbVerieties.COLUMN_VERIETIES_BRAND_ID;
+import static jorje196.com.github.brewmaster.DbContract.DbVerieties.COLUMN_VERIETIES_CAN_ID;
+import static jorje196.com.github.brewmaster.DbContract.DbVerieties.COLUMN_VERIETIES_ID;
+import static jorje196.com.github.brewmaster.DbContract.DbVerieties.COLUMN_VERIETIES_NAME_ID;
+import static jorje196.com.github.brewmaster.DbContract.DbVerieties.TABLE_VERIETIES;
+import static jorje196.com.github.brewmaster.DbContract._COM;
+import static jorje196.com.github.brewmaster.DbContract._PNT;
 
 /**
  * Created by User on 20.12.2017.
- * Отображает список варкок из Db, обеспечивает выбор элемента списка
+ * Отображает список варок из Db, обеспечивает выбор элемента списка
  * для детального просмотра .
  */
 
 public  class BrewListFragment extends ListFragment {
     static final String BLF_TAG = "brewList";    // тег фрагмента
+        // Имена полей в собственном макете элемента списка
+    static final String TEXT_VIEW1_STR = "brewListView1";
+    static final String TEXT_VIEW2_STR = "brewListView2";
+    static final String TEXT_VIEW3_STR = "brewListView3";
+    static final String TEXT_VIEW4_STR = "brewListView4";
 
- String data[] = new String[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
-    "aaa", "bbbbbbbbbbbbbbbbbbbbbb", "ccccccccccccccccccccccccccccccccccccccccc", "dddddddd", "e"};
+    static final String TBD = TABLE_BRANDS + ".";
+    static final String TBW = TABLE_BREWS + ".";
+    static final String TCN = TABLE_CANS + ".";
+    static final String TVR = TABLE_VERIETIES + ".";
+    static final String TNM = TABLE_NAMES + ".";
+    static final String TST = TABLE_STATES + ".";
 
-// ? как передать адаптер , определенный в активности ?
+        // Строка SQL-запроса формирования курсора для работы с фрагментом списка
+        static final String SQL_BREW_LIST2 = "SELECT " + TABLE_BREWS + _PNT + COLUMN_BREWS_ID  + _COM +
+                TABLE_BREWS + _PNT + COLUMN_BREWS_START_DATA  + " AS " + TEXT_VIEW1_STR + _COM +
+                " ( " + TABLE_NAMES + _PNT + COLUMN_NAMES_NAME + "||'   '||" +
+                TABLE_BRANDS + _PNT + COLUMN_BRANDS_BRAND + " ) AS " + TEXT_VIEW2_STR + _COM +
+                " (" + " 'Output ' ||" + TABLE_BREWS + _PNT + COLUMN_BREWS_VOLUME + "||  'л   Alc ' ||" +
+                TABLE_BREWS + _PNT + COLUMN_BREWS_ALC_PERCENT + "||  ' %об.    Bitt ' ||" + "CAST(ROUND" +
+                "(" + TABLE_VERIETIES + _PNT + COLUMN_VERIETIES_BITTER +
+                "*" +  TABLE_CANS + _PNT + COLUMN_CANS_WEIGHT + "/" + TABLE_BREWS + _PNT + COLUMN_BREWS_VOLUME + ", 0) AS INTEGER)" +
+                ") AS " + TEXT_VIEW3_STR + _COM +
+                TABLE_STATES + _PNT + COLUMN_STATES_TEXT  + " AS " + TEXT_VIEW4_STR +
+                " FROM " +
+                TABLE_BREWS +_COM + TABLE_VERIETIES + _COM + TABLE_BRANDS + _COM + TABLE_NAMES + _COM +
+                TABLE_STATES + _COM + TABLE_CANS +
+                " WHERE " +
+                TABLE_BREWS + _PNT + COLUMN_BREWS_VERIETY_ID + "=" + TABLE_VERIETIES + _PNT + COLUMN_VERIETIES_ID + " AND " +
+                TABLE_VERIETIES + _PNT + COLUMN_VERIETIES_BRAND_ID + "=" + TABLE_BRANDS + _PNT + COLUMN_BRANDS_ID + " AND " +
+                TABLE_VERIETIES + _PNT + COLUMN_VERIETIES_NAME_ID + "=" + TABLE_NAMES + _PNT + COLUMN_NAMES_ID + " AND " +
+                TABLE_VERIETIES + _PNT + COLUMN_VERIETIES_CAN_ID + "=" + TABLE_CANS + _PNT + COLUMN_CANS_ID + " AND " +
+                TABLE_STATES + _PNT + COLUMN_STATES_ID + "=" + TABLE_BREWS + _PNT + COLUMN_BREWS_PROCESS_STATE;
+
+    static final String SQL_BREW_LIST = "SELECT " + TBW + COLUMN_BREWS_ID  + _COM +
+        TBW + COLUMN_BREWS_START_DATA  + " AS " + TEXT_VIEW1_STR + _COM +
+        " ( " + TNM + COLUMN_NAMES_NAME + "||'   '||" +
+        TBD + COLUMN_BRANDS_BRAND + " ) AS " + TEXT_VIEW2_STR + _COM +
+        " (" + " 'Output ' ||" + TBW + COLUMN_BREWS_VOLUME + "||  'л   Alc ' ||" +
+        TBW + COLUMN_BREWS_ALC_PERCENT + "||  ' %об.    Bitt ' ||" +
+        "CAST(ROUND(" + TVR + COLUMN_VERIETIES_BITTER + "*" + TCN + COLUMN_CANS_WEIGHT +
+        "/"  + TBW + COLUMN_BREWS_VOLUME + ", 0) AS INTEGER)" + ") AS " + TEXT_VIEW3_STR + _COM +
+        TST + COLUMN_STATES_TEXT  + " AS " + TEXT_VIEW4_STR +
+            " FROM " +
+        TABLE_BREWS +_COM + TABLE_VERIETIES + _COM + TABLE_BRANDS + _COM +
+        TABLE_NAMES + _COM + TABLE_STATES + _COM + TABLE_CANS +
+            " WHERE " +
+        TBW + COLUMN_BREWS_VERIETY_ID + "=" + TVR + COLUMN_VERIETIES_ID + " AND " +
+        TVR + COLUMN_VERIETIES_BRAND_ID + "=" + TBD + COLUMN_BRANDS_ID + " AND " +
+        TVR + COLUMN_VERIETIES_NAME_ID + "=" + TNM + COLUMN_NAMES_ID + " AND " +
+        TVR + COLUMN_VERIETIES_CAN_ID + "=" + TCN + COLUMN_CANS_ID + " AND " +
+        TST + COLUMN_STATES_ID + "=" + TBW + COLUMN_BREWS_PROCESS_STATE;
+
+    /*  nested inner class
+        Класс адаптера курсора, расширяющий SimpleCursorAdapter, создан для получения возможности переписывать методы адаптера курсора,
+    что может быть необходимо для управления отображением элемента списка . Пока пока используется только для выделения текста цветом.
+    Курсор один для всех экземпляров класса фрагмента ! И это правильно.
+     */
+    static class BrewListSimpeCursorAdapter extends SimpleCursorAdapter {
+        Context ctx;
+        BrewListSimpeCursorAdapter(Context context, int layout, Cursor cursor, String[] from, int[] to, int flags ){
+            super(context, layout, cursor, from, to, flags);
+            ctx = context;
+        }
+        int currentTextColor = 0;
+        @Override
+        public void setViewText(TextView v, String text) {
+            super.setViewText(v, text);
+            if (v.getId() == R.id.text4){
+                if (currentTextColor == 0) currentTextColor = v.getCurrentTextColor();
+                if (text.equals(ctx.getResources().getString(R.string.in_progress))){
+                    v.setTextColor(ctx.getResources().getColor(R.color.colorAccentNegative));
+                } else {
+                // Без этого цвет сохраняется при уходе элемента в тень и возвращении , цветовое выделение плодится бесконтрольно
+                    v.setTextColor(currentTextColor);
+                }
+            }
+        }
+    }  // end BrewListSimpeCursorAdapter
+
+    // Интерфейс для связи (передачи информации в родительскую активность
+    interface FragBrewListLink {
+        void getFBLL(ListFragment fragment, String tag);
+    }
+    FragBrewListLink onFragBrewListLink;
+
+    @Override
+    public void onAttach(Activity context) {    // Activity, not Context, else NullPointerException
+        super.onAttach(context);
+        try {
+            onFragBrewListLink = (FragBrewListLink)context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + " must implement onSomeEventListener");
+        }
+
+    }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
-                android.R.layout.simple_list_item_1, data);
-        setListAdapter(adapter);
+        onFragBrewListLink.getFBLL(this, BLF_TAG);
+       // TODO Для исключения утечки памяти надо открывать и закрывать курсоры для адаптера в callback'ах по onActivityCreate и ? onDesrtoy ?
 
     }
     /* Чтобы отображался не layout по умолчанию, а определенный в fragment_brew_list.xml
     Если По умолчанию устраивает, то блок лишний.
-    P.S. android:list & android:empty по определению, заменить нельзя  */
+    P.S. android:list & android:empty по определению, заменить нельзя
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInscanceState){
         return inflater.inflate(R.layout.fragment_brew_list, null);
-    }
+    }      */
+
 
 }
